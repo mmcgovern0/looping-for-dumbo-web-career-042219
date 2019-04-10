51c424963b0ff12ab9bcdcea331f1ef25fa3b7a5
Michael McGovern <mmcgovern.wework@gmail.com>
2019-04-10 04:02:55 +0000

    Automatically backed up by Learn

--- for.rb
+++ for.rb
@@ -1,13 +1,5 @@
 
 def using_for
 	checklist = 1..10
-	#your code here
-end
 
-
-describe "#using_for" do 
-	it "puts until the phrase 'Wingardium Leviosa' 10 times, once for each item on the checklist" do
-		looping_string = "Wingardium Leviosa\nWingardium Leviosa\nWingardium Leviosa\nWingardium Leviosa\nWingardium Leviosa\nWingardium Leviosa\nWingardium Leviosa\nWingardium Leviosa\nWingardium Leviosa\nWingardium Leviosa\n"
-		expect{ using_for }.to output(looping_string).to_stdout
-	end
 end
